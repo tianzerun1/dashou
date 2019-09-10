694251d337894f2bc694498a73f6ce20facbb96a
1278955469@qq.com <1278955469@qq.com>
2019-09-10 17:27:37 +0800

    Initial commit

---
+++ src/main/java/com/liangfutong/dashou/DashouApplication.java
@@ -0,0 +1,13 @@
+package com.liangfutong.dashou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashouApplication.class, args);
+    }
+
+}
